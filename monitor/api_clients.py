fca7e855fc6ccb7d08da41f564331eaa50593f8d
youngbizman <63377152+youngbizman@users.noreply.github.com>
2026-04-25 15:00:43 -0400

    Update api_clients.py

--- monitor/api_clients.py
+++ monitor/api_clients.py
@@ -38,7 +38,7 @@ class ApiClients:
             "apiKey": self.settings.odds_api_key,
             "regions": "eu,us",
             "markets": "h2h,totals,spreads",
-            "bookmakers": "pinnacle,onexbet,draftkings",
+            "bookmakers": "pinnacle,onexbet",
         }
         try:
             data = self._get_json(url, params=params)
